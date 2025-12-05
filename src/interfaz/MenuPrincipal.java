62b6f50f9c75afd7be4a6b49abe4410c9ce2bf96
Víctor Fernández Sáez <130359003+HidiVictr@users.noreply.github.com>
2025-12-05 01:51:06 +0100

    feat: Creation and update application version to v2.2.0.

--- src/interfaz/MenuPrincipal.java
+++ src/interfaz/MenuPrincipal.java
@@ -1205,7 +1205,7 @@ public class MenuPrincipal extends JFrame {
          }
       });
       this.setIconImage((new ImageIcon(this.getClass().getResource("/interfaz/img/k.png"))).getImage());
-      this.setTitle("SWGraphsFIX v2.1.0");
+      this.setTitle("SWGraphsFIX v2.2.0");
       this.setVisible(true);
       this.textoInformacion.requestFocus();
    }
